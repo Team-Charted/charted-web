09fe3d15ef185c6c68ad73ce7675d2bbc9bf90f9
Mizanali Panjwani <mizanalip786@gmail.com>
2021-09-03 03:06:42 +0530

    Add countdown timer

--- src/components/Charts/ChartCard.js
+++ src/components/Charts/ChartCard.js
@@ -3,6 +3,7 @@ import { MdAddCircleOutline, MdEdit } from 'react-icons/md'
 import { IoTrophy, IoPricetag } from 'react-icons/io5'
 import axios from 'axios'
 import { Link, Redirect, withRouter } from 'react-router-dom'
+import Countdown from 'react-countdown'
 
 const ChartCard = ({ chartId, chartName, prizePool, cost, date, endTime }) => {
 
@@ -23,11 +24,13 @@ const ChartCard = ({ chartId, chartName, prizePool, cost, date, endTime }) => {
         }
     }, [])
 
-    return (    
+    return (
         <div className='mx-auto w-full h-52'>
             <div className={`h-3/4 p-4 flex flex-col justify-around rounded-t-xl bg-${chartName.split(' ')[0].toLowerCase()}`}>
                 <h1 className='font-bold text-lg text-center'>{chartName}</h1>
-                <h3 className='font-semibold text-lg text-center'>{endTime}</h3>
+                <Countdown className='font-semibold text-lg text-center' date={Date.parse(endTime)} daysInHours={true}>
+                    <h3 className='font-semibold text-lg text-center'>Chart Locked</h3>
+                </Countdown>
                 <div className='w-full flex justify-between'>
                     <span className=''><IoTrophy className='inline' /> {prizePool}</span>
                     <span className=''><IoPricetag className='inline' /> {cost}</span>
@@ -37,7 +40,7 @@ const ChartCard = ({ chartId, chartName, prizePool, cost, date, endTime }) => {
                 <span>{date}</span>
                 {!loading && <Link to={`/charts/${chartId}`}>{album.length == 0 ? <MdAddCircleOutline className='text-lg' /> : <MdEdit className='text-lg' />}</Link>}
             </div>
-        </div>
+        </div >
     )
 }
 
